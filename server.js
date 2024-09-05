a89da04ab74c2d0f7715fea2e98326a8dc184281
Alfredo Pasquel <alfredo.pasquel@gmail.com>
2024-09-05 14:03:52 -0400

    updated server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
 const middlewares = jsonServer.defaults();
 
+// Middleware setup
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
+// Define the PORT and bind to 0.0.0.0
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
